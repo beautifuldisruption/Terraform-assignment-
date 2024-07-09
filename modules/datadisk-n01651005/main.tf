91aa0060d846763656a463f4f1d76fece33396b8
Aromo Oluwamijuwon <89385255+beautifuldisruption@users.noreply.github.com>
2024-07-09 01:05:58 -0400

    Update main.tf

--- modules/datadisk-n01651005/main.tf
+++ modules/datadisk-n01651005/main.tf
@@ -13,5 +13,5 @@ resource "azurerm_virtual_machine_data_disk_attachment" "data_disk_attachment" {
   managed_disk_id    = azurerm_managed_disk.data_disk[count.index % length(azurerm_managed_disk.data_disk)].id
   virtual_machine_id = element(concat(var.linux_vm_ids, var.windows_vm_ids), count.index)
   lun                = count.index
-  caching            = "ReadWrite"  # Add the caching argument here
+  caching            = "ReadWrite"  
 }
